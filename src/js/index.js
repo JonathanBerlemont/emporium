cfe1671875c3a63b36c47283778eaa84ecd75674
Jonathan Berlemont <berlemontjonathan@gmail.com>
2019-02-17 17:39:15 +0100

    Ajout de navbar avec bouton

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 /*
---------------------------------------------------------------
-Modal de connection
---------------------------------------------------------------
+|--------------------|
+|Modal de connection |
+|--------------------|
 */
 let modal = document.getElementById("modal");
 let connection_button = document.querySelector("nav button");
@@ -13,13 +13,15 @@ let body = document.getElementsByTagName("body")[0];
 
 let modal_inputs = [...modal.getElementsByTagName("input")];
 
-//Affichage du modal
 connection_button.addEventListener("click", () => {
     let first_input = modal.getElementsByTagName("input")[0];
+    
+    //Affichage du modal
     modal.classList.remove("d-none");
-
+    
+    //met le focus sur le premier input du modal, pour eviter d'appuyer sur "enter" apres avoir cliquer sur le bouton connexion et de créer un nombre indésirable de div "blackness"
     first_input.focus();
-
+    
     //rajoute un effet à l'apparition
     modal.classList.add("popIn");
     
@@ -29,15 +31,13 @@ connection_button.addEventListener("click", () => {
     blackness.style.width = "100%";
     blackness.style.position = "fixed";
     blackness.style.top = "0";
-    blackness.style.zIndex = "9";
+    blackness.style.zIndex = "10";
     blackness.style.backgroundColor = "black";
     blackness.style.opacity = ".7";
-
     body.insertBefore(blackness, body.firstChild);
+    
     //Empeche le scroll pendant que le modal est ouvert
     body.style.overflow = "hidden";
-    
-    
 })
 
 //Fonctionalité du close button
@@ -45,30 +45,34 @@ let close_button = document.getElementById("close-button");
 close_button.addEventListener("click", () => {
     //rajoute un effet avant de disparaitre
     modal.classList.add("popOut");
-
+    
+    //le timeout sert à avoir le temps de voir l'animation avant que la div ne disparaisse
     setTimeout( () => {
         modal.classList.add("d-none");
-
-        //eneleve les effets pour qu'ils puissent etre réutilisés après
+        
+        //enleve les effets pour qu'ils puissent etre réutilisés après
         modal.classList.remove("popIn");
         modal.classList.remove("popOut");
-
+        
         //enleve le background assombri
         body.removeChild(body.firstChild);
+        
         //reactive le scrolling de la page
         body.style.overflow = "";
-
+        
+        //fait en sorte que lorsque l'on quitte le modal depuis l'inscription et qu'on re-ouvre le modal, on soit sur l'onglet connexion
         if (!div_inscription.classList.contains("d-none")){
             div_inscription.classList.add("d-none");
             div_connection.classList.remove("d-none");
         }
-
+        
+        //vide les input après fermeture du modal
         modal_inputs.forEach((element) => {
             element.value = "";
         })
-
+        
     }, 300)
-
+    
 })
 
 //switch entre connection et inscription
@@ -80,18 +84,18 @@ for (let i = 0; i<switch_button.length; i++){
 }
 
 /*
---------------------------------------------------------------
-Fin du modal de connection
---------------------------------------------------------------
+|---------------------------|
+|Fin du modal de connection |
+|---------------------------|
 */
 
 
 
 
 /*
---------------------------------------------------------------
-Changement entre dark mode et light mode
---------------------------------------------------------------
+|-----------------------------------------|
+|Changement entre dark mode et light mode |
+|-----------------------------------------|
 */
 let dark_mode = document.getElementById("dark-mode");
 let light_mode = document.getElementById("light-mode");
@@ -119,9 +123,9 @@ light_mode.addEventListener("click", () => {
     navbar_h1.style.color = "black";
 })
 /*
---------------------------------------------------------------
-Fin du changement entre dark mode et light mode
---------------------------------------------------------------
+|------------------------------------------------|
+|Fin du changement entre dark mode et light mode |
+|------------------------------------------------|
 */
 
 
@@ -129,28 +133,74 @@ Fin du changement entre dark mode et light mode
 
 
 /*
---------------------------------------------------------------
-Changement de la navbar pendant le scroll
---------------------------------------------------------------
+|------------------------------------------|
+|Changement de la navbar pendant le scroll |
+|------------------------------------------|
 */
 
 let nav = document.getElementById("navbar");
 let section_top = document.getElementById("section-top");
-let added_padding = nav.clientHeight;
+
+//rajout de padding à la transformation de la navbar pour eviter que la page ne remonte brusquement
+let nav_height = nav.clientHeight;
+
+let burger = document.getElementById("burger");
+let navbar_content = document.getElementsByTagName("nav")[0];
+
+section_top.style.paddingTop = nav_height+ "px";
 
 window.addEventListener("scroll", () => {
-    if (document.documentElement.scrollTop > 200) {
+    if (document.documentElement.scrollTop > nav_height) {
         nav.classList.add("navbar-scroll");
-        section_top.style.paddingTop = added_padding+ "px";
-
+        
     } else {
         nav.classList.remove("navbar-scroll");
-        section_top.style.paddingTop = 0;
     }
 })
 
+//Apparition du contenu de la navbar
+burger.addEventListener("click", () => {
+    navbar_content.classList.toggle("shown");
+    
+    if(navbar_content.classList.contains("shown")){
+        //quand on veut montrer la navbar
+        navbar_content.classList.remove("getOut");
+        navbar_content.classList.add("getIn");
+        burger.firstChild.className = "fas fa-angle-double-left";
+        
+        //Crée un assombrissement du background pendant que le menu est ouvert
+        let blackness = document.createElement("div");
+        blackness.style.height = "150vh";
+        blackness.style.width = "100%";
+        blackness.style.position = "fixed";
+        blackness.style.top = "0";
+        blackness.style.zIndex = "10";
+        blackness.style.backgroundColor = "black";
+        blackness.style.opacity = ".7";
+        body.insertBefore(blackness, body.firstChild);
+        
+        //Empeche le scroll pendant que le menu est ouvert
+        body.style.overflow = "hidden";
+        
+    } else {
+        //quand on veut cacher la navbar
+        body.removeChild(body.firstChild);
+        body.style.overflow = "";
+        navbar_content.classList.remove("getIn");
+        navbar_content.classList.add("getOut");
+        burger.firstChild.className = "fas fa-angle-double-right";
+
+        setTimeout(() => {
+            navbar_content.classList.remove("getOut");
+        }, 1000);
+    }
+    
+})
+
 /*
---------------------------------------------------------------
-Fin changement de la navbar pendant le scroll
---------------------------------------------------------------
+________________________________________________
+|                                              |
+|Fin changement de la navbar pendant le scroll |
+|______________________________________________|
+
 */
