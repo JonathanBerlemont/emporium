2a4a592f19a77927fb6eb52b73dec9411ac78a4f
Jonathan Berlemont <berlemontjonathan@gmail.com>
2019-02-19 10:20:19 +0100

    Ajout du carousel

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,11 @@
+//Imports des scripts
 import {setModal} from "./partials/_modal.js";
 import {setDarkLight} from "./partials/_dark_light.js";
 import {setScroll} from "./partials/_scroll.js";
+import {setCarousel} from "./partials/_carousel.js";
 
+//Appel des scripts
 setModal();
 setDarkLight();
 setScroll();
+setCarousel();
